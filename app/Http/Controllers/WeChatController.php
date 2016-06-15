572959f5977413f8be825b9deba39b7241079463
zhou <304863859@qq.com>
2016-06-15 17:42:22 +0800

    a

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use EasyWeChat\Message\Text;
 use Log;
 class WeChatController extends Controller
 {
@@ -23,7 +24,7 @@ class WeChatController extends Controller
             if ($message->MsgType == 'event') {
                 switch ($message->Event) {
                     case 'subscribe':
-                        return '欢迎关注!';
+                        return $this->handleEvent($message);
                         break;
 
                     default:
@@ -31,9 +32,10 @@ class WeChatController extends Controller
                         break;
                 }
             }elseif($message->MsgType == 'voice') {
-                return "不好意思，小探不是AlphaGo，听不懂呢！";
+                $str="不好意思，小探不是AlphaGo，听不懂呢！";
+                return  new Text(['content'=>$str]);
             }
-            return "欢迎来到最有逼格的民宿文化社群！\n\n";
+
         });
 
         Log::info('return response.');
@@ -44,5 +46,72 @@ class WeChatController extends Controller
     public function menus(){
         $wechat = app('wechat');
         dd($wechat->menu->current());
+    }
+
+
+    public function handleEvent($obj)
+    {
+        $wxId = $obj->FromUserName;
+        $content = '';
+        switch ($obj->Event) {
+            case "subscribe":   //关注事件
+                $content = $wxId."请注册，注册后即可快速查询水电费\n<a href='".action('WeChatController@menus')."'>点击这里，立即注册</a>\n";
+                break;
+            case "unsubscribe": //取消关注事件
+                $content = "";
+                break;
+            case 'CLICK': {
+                switch ($obj->EventKey) {
+                    case '天然气咨询':
+                        $content = [
+                            "title" => "管道天然气来啦！",
+                            "description" => "关于安装管道天然气的意见征询的通知。",
+                            "image" => "http://www.hcfsz.com/cf/images/trq.jpg",
+                            "url" => "http://www.hcfsz.com/cf/trq/trqyj.php?wxId=$wxId"
+                        ];
+                        break;
+                    case '水电费查询':
+                        if($this->isSignUp($wxId)){
+                            $content = '直接输入时间即可查询您该月的水电详情，如（201509）。';
+                        }else{
+                            $content = "请注册，注册后即可快速查询水电费\n<a href='".action('WechatController@signUp', ['wxId'=>$wxId])."'>点击这里，立即注册</a>\n";
+                        }
+                        break;
+                    case '天气预报':
+                        $content = "请输入城市名+天气，如: 深圳天气";
+                        break;
+                    case '意见反馈':
+                        if($this->isSignUp($wxId)){
+                            $content = [
+                                'title'=>'反馈村夫',
+                                'description'=>'您的意见是我们前进的动力。',
+                                'image'=>asset('img/feedback/shakehand.jpg'),
+                                'url'=>action('WechatController@feedback', ['wxId'=>$wxId])//url('/feedback',['wxId'=>$wxId]),
+                            ];
+                        }else{
+                            $content = "请注册，注册后即可快速查询水电费\n<a href='".action('WechatController@signUp', ['wxId'=>$wxId])."'>点击这里，立即注册</a>\n";
+                        }
+                        break;
+                    case '利生活':
+                        $content = "更多精彩，敬请期待。";
+                        break;
+                    case '停车缴费':
+                        $content = '敬请期待';
+                        break;
+                    default:
+                        $content = "敬请期待";
+                        break;
+                }
+                break;
+            }
+            default:
+                break;
+        }
+        if(is_array($content)){
+            $ret = new News($content);
+        }else{
+            $ret = new Text(['content'=>$content]);
+        }
+        return $ret;
     }
 }
